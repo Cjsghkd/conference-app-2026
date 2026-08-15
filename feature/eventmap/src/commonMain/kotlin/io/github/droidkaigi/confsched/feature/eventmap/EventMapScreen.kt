25b15551122f5184d40875b8cc40884fb7876e50
Jihyun-Park0 <jihyun.park.private@gmail.com>
2026-08-15 23:06:15 +0900

    Add event items

--- feature/eventmap/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/eventmap/EventMapScreen.kt
+++ feature/eventmap/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/eventmap/EventMapScreen.kt
@@ -1,12 +1,15 @@
 package io.github.droidkaigi.confsched.feature.eventmap
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.plus
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -19,6 +22,8 @@ import io.github.droidkaigi.confsched.core.preview.KaigiSchemeProvider
 import io.github.droidkaigi.confsched.core.preview.LocalePreviews
 import io.github.droidkaigi.confsched.core.preview.wrapper.KaigiPreviewTheme
 import io.github.droidkaigi.confsched.core.ui.KaigiTopAppBar
+import io.github.droidkaigi.confsched.core.ui.SketchHorizontalDivider
+import io.github.droidkaigi.confsched.feature.eventmap.component.EventItem
 import io.github.droidkaigi.confsched.feature.eventmap.component.FloorMapCard
 import io.github.droidkaigi.confsched.feature.eventmap.component.FloorTabRow
 import io.github.droidkaigi.confsched.feature.eventmap.component.StampRallyCard
@@ -68,6 +73,24 @@ fun EventMapScreen(
                     onLearnMoreClick = { /*TODO*/ },
                 )
             }
+            items(uiState.eventMapItems) { event ->
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(20.dp),
+                ) {
+                    EventItem(
+                        event = event,
+                    )
+                    if (event != uiState.eventMapItems.last()) {
+                        SketchHorizontalDivider(
+                            seed = 12,
+                            thickness = 1.3.dp,
+                            color = MaterialTheme.colorScheme.outlineVariant,
+                            modifier = Modifier
+                                .height(5.dp),
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -78,8 +101,12 @@ private fun EventMapScreenPreview(
     @PreviewParameter(KaigiSchemeProvider::class) colorScheme: KaigiColorScheme,
 ) {
     KaigiPreviewTheme(colorScheme) {
+        val selectedFloor = EventMapFloor.Ground
         EventMapScreen(
-            uiState = EventMapScreenUiState(selectedFloor = EventMapFloor.Ground),
+            uiState = EventMapScreenUiState(
+                selectedFloor = selectedFloor,
+                eventMapItems = EventMapScreenUiState.mock(selectedFloor),
+            ),
             onFloorClick = {},
         )
     }
