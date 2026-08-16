720fb5bc00362c567a7ba583923bed27bf9e7d21
Jihyun-Park0 <jihyun.park.private@gmail.com>
2026-08-16 14:42:02 +0900

    Move RoomChip.kt to core/ui from feature/timetable

--- feature/eventmap/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/eventmap/component/EventItem.kt
+++ feature/eventmap/src/commonMain/kotlin/io/github/droidkaigi/confsched/feature/eventmap/component/EventItem.kt
@@ -17,6 +17,7 @@ import io.github.droidkaigi.confsched.core.preview.KaigiSchemeProvider
 import io.github.droidkaigi.confsched.core.preview.LocalePreviews
 import io.github.droidkaigi.confsched.core.preview.wrapper.KaigiPreviewTheme
 import io.github.droidkaigi.confsched.core.ui.KaigiOutlinedButton
+import io.github.droidkaigi.confsched.core.ui.RoomChip
 import io.github.droidkaigi.confsched.feature.eventmap.EventMapItem
 import io.github.droidkaigi.confsched.feature.eventmap.generated.resources.Res
 import io.github.droidkaigi.confsched.feature.eventmap.generated.resources.event_map_learn_more_button_label
